e475411d8eb3c554fd6ca48315e8f6e0e73034b2
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-09-27 17:27:35 +0100

    passing info from the process to the ui

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,15 +1,20 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
-import { isDev } from "./util.js"
+import { isDev } from "./util.js";
 import { pollResources } from "./resourceManager.js";
+import { getPreloadPath } from "./pathResolver.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath()
+    },
+  });
   if (isDev()) {
-    mainWindow.loadURL("http://localhost:5123")
+    mainWindow.loadURL("http://localhost:5123");
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
 
-  pollResources()
+  pollResources();
 });
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,7 +3,10 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // @ts-ignore
+  window.electron.getStaticData();
 
   return (
     <>
