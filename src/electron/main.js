a332e663b5b0ee20b522a7716eedd17fedcd4645
Ekomobong Archibong <59149177+archie-bryann@users.noreply.github.com>
2025-09-27 13:08:45 +0100

    setting up electron with typescript

--- src/electron/main.js
+++
@@ -1,6 +0,0 @@
-import { app, BrowserWindow } from "electron";
-
-app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
-  mainWindow.loadFile(app.getAppPath() + "/dist-react/index.html");
-});
